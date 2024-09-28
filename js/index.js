745a28c66c874a81a556df6698e12699897eda00
Antonio Cuadrado Valdez <antoniocua2016@gmail.com>
2024-09-28 20:46:06 -0300

    funcion para validar si el campo esta vacio y una funciona para calcular el costo

---
+++ js/index.js
@@ -0,0 +1,32 @@
+function validarCampo(sesion, horas) {
+  sesion = prompt("Ingrese nombre de la sesion");
+  horas = parseInt(prompt("Ingrese cuantas horas quieres"));
+
+  if (sesion.trim() === "") {
+    while (sesion.trim() === "") {
+      sesion = prompt(
+        "El campo está vacío. Por favor, ingrese nombre de la sesion"
+      );
+    }
+  } else if (isNaN(horas)) {
+    while (isNaN(horas)) {
+      horas = parseInt(
+        prompt("El campo está vacío. Ingrese cuantas horas quieres")
+      );
+    }
+  }
+
+  alert("La sesion elegida es " + sesion + " y durara " + horas + " hs");
+  return horas;
+}
+
+function calcularCosto() {
+  const PRECIO = 7000;
+  let horas = validarCampo();
+  let total = horas * PRECIO;
+
+  alert("El total de la sesion es " + "$" + total);
+}
+
+
+calcularCosto();
